a49f4a0a2a37b539cc774233c6df7f35162a2958
gaga <moonsky862@gmail.com>
2024-11-20 14:37:10 +0800

    refactor(debug): hide the debug button

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,30 @@
 <template>
 	<main>
 		<div class="controller-and-score">
-<!--			<game-score :score="score" :max_score="max_score" />-->
-<!--			<game-pad class="gamepad" />-->
-			<button @click="game.stop()">STOP</button>
+			<game-score :score="score" :max_score="max_score" />
+			<game-pad class="gamepad" />
+			<!--			<button @click="game.stop()">STOP</button>-->
 		</div>
 		<div class="game" id="game"></div>
 		<div class="next-and-sample">
 			<div id="next"></div>
-<!--			<game-sample class="game-sample" />-->
+			<game-sample class="game-sample" />
 		</div>
 	</main>
 	<the-welcome v-if="is_show_welcome" @click="startGame" class="welcome" />
 	<game-over v-if="is_game_over" />
-	<score-tooltip :score="new_score" :left="new_score_left" :top="new_score_top"/>
+	<score-tooltip :score="new_score" :left="new_score_left" :top="new_score_top" />
 </template>
 
 <script setup lang="ts">
 import TheWelcome from "@/components/TheWelcome.vue";
 import { Game } from "@/game/game";
-import { computed, onMounted, onUnmounted, ref } from "vue";
+import { onMounted, onUnmounted, ref } from "vue";
 import GameSample from "@/components/GameSample.vue";
 import GamePad from "@/components/GamePad.vue";
 import GameScore from "@/components/GameScore.vue";
 import { GAME_BOARD_CELL_SIZE, GAME_BOARD_COL, GAME_BOARD_ROW } from "@/game/config";
-import {
-	getElementScreenPosition,
-	getHistoryMaxScore,
-	getSquareCenterPixelPosition,
-	setHistoryMaxScore
-} from "@/utils/utils";
+import { getElementScreenPosition, getHistoryMaxScore, getSquareCenterPixelPosition, setHistoryMaxScore } from "@/utils/utils";
 import GameOver from "@/components/GameOver.vue";
 import ScoreTooltip from "@/components/ScoreTooltip.vue";
 import type { BevelledSquare, NormalSquare } from "@/game/types";
@@ -44,7 +39,6 @@ const shooo_audio = new Audio("/audio/shooo.mp3");
 const new_score_top = ref(100);
 const new_score_left = ref(100);
 
-
 let game: Game;
 
 function startGame() {
@@ -52,8 +46,7 @@ function startGame() {
 	game.start();
 }
 
-window.document.addEventListener("click", () => {
-});
+window.document.addEventListener("click", () => {});
 
 function onScore(gain: number, square: NormalSquare | BevelledSquare) {
 	console.log(square);

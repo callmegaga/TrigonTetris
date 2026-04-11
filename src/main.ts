fa175d1a7d2f368c4d3cd273591ee885f28286b9
callmegaga <moon3sky@gmail.com>
2026-04-11 22:41:32 +0800

    (build): setup vercel speed insight and analytics

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,9 @@
 import "./assets/style/main.css";
 import { createApp } from "vue";
+import { inject as injectAnalytics } from "@vercel/analytics";
+import { injectSpeedInsights } from "@vercel/speed-insights";
 import App from "./App.vue";
 
+injectAnalytics();
+injectSpeedInsights();
 createApp(App).mount("#app");
-
-

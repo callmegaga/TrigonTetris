97aea93ac79bc9badb0fb42de3e56aaef52c0fb3
callmegaga <moon3sky@gmail.com>
2026-04-11 21:58:28 +0800

    (build): remove base url

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,6 @@ import vueDevTools from "vite-plugin-vue-devtools";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	base: "/TrigonTetris/",
 	plugins: [vue(), vueJsx(), vueDevTools()],
 	resolve: {
 		alias: {

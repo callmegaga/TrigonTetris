ae544283595aaede7b33b4f375c800d6406cc2e8
callmegaga <moon3sky@gmail.com>
2026-05-12 22:05:43 +0800

    feat(block): change shape-5 block again

--- src/game/blocks/shape-5.ts
+++ src/game/blocks/shape-5.ts
@@ -4,9 +4,8 @@ import { CellValue, type Shape } from "@/game/types";
 export class Shape5 extends Block {
 	constructor() {
 		const shape: Shape = [
-			[CellValue.Empty, CellValue.Full, CellValue.Full],
-			[CellValue.Empty, CellValue.TriangleRightTop, CellValue.Full],
-			[CellValue.Empty, CellValue.Empty, CellValue.TriangleRightTop]
+			[CellValue.Empty, CellValue.TriangleRightBottom, CellValue.TriangleLeftBottom, CellValue.Empty],
+			[CellValue.Full, CellValue.Full, CellValue.Full, CellValue.TriangleLeftBottom]
 		];
 
 		super(shape, "#f799fc");
